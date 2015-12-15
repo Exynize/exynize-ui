24fe1e7fe8d22b520f7bcb169ef3db2c1d6c6e0f
Tim Ermilov <yamalight@gmail.com>
2015-12-15 13:09:52 +0100

    deploy in docker using express.js and proxy requests to rest api internally

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,20 @@
-FROM nginx
+FROM node
 
-COPY . /usr/share/nginx/html
-COPY docker/default.conf /etc/nginx/conf.d/default.conf
+# Define working directory
+WORKDIR /opt/app
+
+# Install top dependencies w/ caching
+COPY package.json /opt/app/package.json
+RUN npm install --silent
+
+# Bundle source
+COPY . /opt/app
+
+# Pre-build app
+RUN NODE_ENV=production node build.js
+
+# Expose port 3000
+EXPOSE 3000
+
+# Define default command.
+CMD ["node", "serve.js"]
---
+++ serve.js
@@ -0,0 +1,31 @@
+// start webpack
+const path = require('path');
+const express = require('express');
+const proxyMiddleware = require('http-proxy-middleware');
+
+// configure proxy middleware context
+const context = '/api'; // requests with this path will be proxied
+// configure proxy middleware options
+const options = {
+    target: process.env.NODE_ENV === 'production' ? 'http://exynize-rest:8080' : 'http://localhost:8080', // target host
+    changeOrigin: true,               // needed for virtual hosted sites
+    ws: true,                         // proxy websockets
+};
+// create the proxy
+const proxy = proxyMiddleware(context, options);
+
+// create express
+const app = express();
+app.use(express.static(__dirname));
+app.use(proxy);
+// serve index
+app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+// start server
+app.listen(3000, function onStart(err) {
+    if (err) {
+        console.log(err);
+    }
+    console.info('==> Listening on port 3000');
+});
